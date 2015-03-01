859fc54972345a79627fdb76e3c029f442b03e2e
植草 智輝 <tmkueks19@gmail.com>
2015-03-01 12:35:55 +0900

    ファイルの追加しわすれの修正

---
+++ JumperChampion/Assets/Script/AnimationController_ver4_1.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationController_ver4_1 : MonoBehaviour {
+	Animator animator;
+	
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+		Debug.Log (animator);
+		GameObject go = GameObject.Find("Board");
+		go.SendMessage("setAnimator",animator);
+		Debug.Log ("set animator");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			//AnimateJumping();
+		}
+		if (Input.GetKeyDown(KeyCode.B)) {
+			//AnimateLeftSlide();
+		}
+	}
+	
+	public void AnimateJumping(){
+		Debug.Log ("animate jumping");
+		animator.SetTrigger("toJumping");
+	}
+	public void AnimateLeftSlide(){
+		animator.SetTrigger("toLeftSlide");
+	}
+	public void AnimateRightSlide(){
+		animator.SetTrigger("toRightSlide");
+	}
+	public void AnimateFalling() {
+		animator.SetTrigger("toFalling");
+	}
+}
